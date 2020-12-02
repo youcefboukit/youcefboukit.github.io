574d374a8b215b5f8aaa4c5d8087aaad4a7ae6f2
Youcef.B <youcefboukit1@gmail.com>
2020-12-02 18:06:44 +0100

    Create fcmtest.js

---
+++ fcmtest.js
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>FCM test</title>
+    </head>
+    <body>
+        <h1>online</h1>
+        
+        
+        <!-- The core Firebase JS SDK is always required and must be listed first -->
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js"></script>
+
+<!-- TODO: Add SDKs for Firebase products that you want to use
+     https://firebase.google.com/docs/web/setup#available-libraries -->
+<script src="https://www.gstatic.com/firebasejs/8.1.1/firebase-analytics.js"></script>
+
+<script>
+
+console.log('script');
+  // Your web app's Firebase configuration
+  // For Firebase JS SDK v7.20.0 and later, measurementId is optional
+  var firebaseConfig = {
+    apiKey: "AIzaSyBM7SLB6bhL-Vglrf3fYUDjkNID9MGQrmw",
+    authDomain: "khedma-335f8.firebaseapp.com",
+    databaseURL: "https://khedma-335f8.firebaseio.com",
+    projectId: "khedma-335f8",
+    storageBucket: "khedma-335f8.appspot.com",
+    messagingSenderId: "509617335156",
+    appId: "1:509617335156:web:d1c62fe6bb05d35f0c5a21",
+    measurementId: "G-R3Y1598KTT"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+  console.log('everything working?');
+  
+ // firebase.analytics();
+</script>
+
+    </body>
+</html>
